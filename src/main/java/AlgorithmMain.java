4c7b633ee988340c7659643aebf54d378c32ead3
min <min52@malgnsoft.com>
2022-10-20 22:25:44 +0900

    DFS,BFS week2

--- src/main/java/AlgorithmMain.java
+++ src/main/java/AlgorithmMain.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class AlgorithmMain {
+    Node root;
     public static void main(String[] args) {
         System.out.println("시작!");
 //        System.out.println(Arrays.toString(Sorting.sorting3(new int[] {11, 7, 5, 6, 10, 9})));
@@ -11,7 +12,16 @@ public class AlgorithmMain {
 //        System.out.println(Sorting.sorting7(new int[][] {{2, 7}, {1, 3}, {1, 2}, {2, 5}, {3, 5}}));
 
 //        Recursive.recursive1(3);
-        System.out.println(Recursive.recursive3(5));
+//        System.out.println(Recursive.recursive3(5));
 //        System.out.print(Recursive.recursive4and1(10));
+        AlgorithmMain tree = new AlgorithmMain();
+        tree.root = new Node(1);
+
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        System.out.println(Recursive.recursive10(tree.root));
     }
 }
--- src/main/java/Recursive.java
+++ src/main/java/Recursive.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Recursive {
     public static void recursive1(int n) {
@@ -21,5 +23,31 @@ public class Recursive {
         if(n == 1) return 1;
         else if(n == 2) return 1;
         else return recursive4and1(n-2) + recursive4and1(n-1);
+    }
+
+    public static int recursive10(Node root){
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        int level = 0;
+        while(!q.isEmpty()) {
+            int len = q.size();
+            for(int i = 0; i < len; i++) {
+                Node current = q.poll();
+                if(current.lt == null && current.rt == null) return level;
+                if(current.lt != null) q.offer(current.lt);
+                if(current.rt != null) q.offer(current.rt);
+            }
+            level++;
+        }
+        return 0;
+    }
+}
+
+class Node {
+    int data;
+    Node lt, rt;
+    public Node(int val) {
+        data = val;
+        lt = rt = null;
     }
 }
